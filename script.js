896fec2e431c6b7ff1da7ee69050c03f9542acfb
Venkatesh <venkateshpk06@gmail.com>
2024-01-30 21:38:21 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//Sum of all numbers in an array
+//1.Print Odd numbers in an Array
+var arr = [12,78,45,65];
+for(var i=0;i<arr.length;i++){
+    if(arr[i]%2 != 0){
+        console.log(arr[i])
+    }
+}
+
+
+//3.Sum of all numbers in an array
 var arr = [12,78,45,65];
 var sum = 0
 for (var i=0;i<arr.length;i++){
